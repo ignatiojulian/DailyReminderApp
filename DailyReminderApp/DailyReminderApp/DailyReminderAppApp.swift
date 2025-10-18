f15cdbbfdeb30d81eade6b3ea3e31488ae122e3b
Ignatio Julian <ignatiojulian@gmail.com>
2025-10-18 10:09:58 +0700

    init: project ios

---
+++ DailyReminderApp/DailyReminderApp/DailyReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyReminderAppApp.swift
+//  DailyReminderApp
+//
+//  Created by Ignatio Julian on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyReminderApp/DailyReminderAppTests/DailyReminderAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyReminderAppTests.swift
+//  DailyReminderAppTests
+//
+//  Created by Ignatio Julian on 18/10/25.
+//
+
+import Testing
+@testable import DailyReminderApp
+
+struct DailyReminderAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
